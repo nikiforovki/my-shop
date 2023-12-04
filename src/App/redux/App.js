491cd025448ed3af747417ce18c7a94984c36bc1
nikiforovki <auto.nickiforov94@gmail.com>
2023-12-04 15:17:28 +0200

    feature/stock

---
+++ src/App/redux/App.js
@@ -0,0 +1,16 @@
+import Heder from "../../components/Header";
+import Main from "../../components/Main"
+import Footer from "../../components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+<div className="wrapper">
+  <Heder />
+  <Main />
+  <Footer />
+</div>
+  );
+}
+
+export default App;
