b4e753b713dfc15bf4e59862434d951639d049af
nikiforovki <auto.nickiforov94@gmail.com>
2023-12-07 01:37:38 +0200

    feature/lk

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
-import { useLocation, useSearchParams } from 'react-router-dom';
-import useLocalStorage from '../useLocalStorage.js';
+import { useLocation, useSearchParams, useNavigate } from 'react-router-dom';
+import useLocalStorage from './loginPage/useLocalStorage.js';
 
 const Profile = () => {
  const location = useLocation();
+ const navigate = useNavigate(); // Импортируем useNavigate вместо useHistory
  const [orders, setOrders] = useLocalStorage('orders', []); // Используем useLocalStorage вместо useState
  const [user, setUser] = useLocalStorage('user', null); // Используем кастомный хук
  const [searchParams, setSearchParams] = useSearchParams();
@@ -24,21 +25,27 @@ const Profile = () => {
  setSearchParams(searchParams);
  };
 
+ const handleLogout = () => {
+ setUser(null); // Обнуляем состояние user
+ navigate('/'); // Перенаправляем пользователя на главную страницу
+ };
+
  return (
-   <div>
-     <h1>Proff</h1>
-     {user && <p>Welcome, {user.name}</p>} {/* Отображаем имя пользователя */}
-     <h2>Заказы</h2>
-     <ul>
-       {orders.map(order => (
-         <li key={order.id}>
-           {order.description}
-           <button onClick={() => handleDelete(order.id)}>Отменить</button>
-         </li>
-       ))}
-     </ul>
-   </div>
+ <div>
+  {user && <p>Welcome, {user.name}</p>} {/* Отображаем имя пользователя */}
+  <button onClick={handleLogout}>Выход</button> {/* Добавляем функцию выхода */}
+  <h2>Заказы</h2>
+  <ul>
+    {orders.map(order => (
+      <li key={order.id}>
+        {order.description}
+        <button onClick={() => handleDelete(order.id)}>Отменить</button>
+      </li>
+    ))}
+  </ul>
+ </div>
  ); 
 };
 
 export default Profile;
+
--- src/Pages/loginPage/Login.js
+++ src/Pages/loginPage/Login.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import useLocalStorage from './useLocalStorage';
+
 
 // Объект с данными пользователей
 const users = {
-  User1: { name: 'User1', password: 'pass1' },
-  User2: { name: 'User2', password: 'pass2' },
- };
- 
+ User1: { name: 'User1', password: 'pass1' },
+ User2: { name: 'User2', password: 'pass2' },
+};
 
 const Login = () => {
  const [username, setUsername] = useState('');
@@ -14,36 +15,39 @@ const Login = () => {
  const navigate = useNavigate();
 
  const handleInputChange = (event) => {
- if (event.target.name === 'username') {
-  setUsername(event.target.value);
- } else if (event.target.name === 'password') {
-  setPassword(event.target.value);
+  if (event.target.name === 'username') {
+    setUsername(event.target.value);
+  } else if (event.target.name === 'password') {
+    setPassword(event.target.value);
+  }
  }
- }
- 
+
+ const [user, setUser] = useLocalStorage('user', null);
+
  const handleSubmit = (event) => {
   event.preventDefault();
   if (users[username] && users[username].password === password) {
+    setUser(users[username]); // Обновляем состояние user
     navigate('/Profile');
   } else {
     alert('Неверные учетные данные');
   }
  }
- 
- 
+
+
 
  return (
- <form style={{ paddingTop: '50px', marginLeft: '20px' }} onSubmit={handleSubmit}>
-  <label>
-    Username:
-    <input type="text" name="username" onChange={handleInputChange} />
-  </label>
-  <label>
-    Password:
-    <input type="password" name="password" onChange={handleInputChange} />
-  </label>
-  <input type="submit" value="Submit" />
- </form>
+  <form style={{ paddingTop: '50px', marginLeft: '20px' }} onSubmit={handleSubmit}>
+    <label>
+      Username:
+      <input type="text" name="username" onChange={handleInputChange} />
+    </label>
+    <label>
+      Password:
+      <input type="password" name="password" onChange={handleInputChange} />
+    </label>
+    <input type="submit" value="Вход" />
+  </form>
  );
 }
 
